96acb85644b1e9a85cdea32fc755d4df7dafa727
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-26 10:45:52 -0300

    update config.json path and keys

--- manager/utils/index.js
+++ manager/utils/index.js
@@ -1,12 +1,12 @@
 let config;
 
 try {
-    config = require('../config.json');
+    config = require('../../config.json');
 } catch (error) {
     config = null;
 }
 
-exports.DISCORD = config ? config.discord : process.env.DISCORD;
-exports.GENERAL = config ? config.general : process.env.GENERAL;
-exports.SHORT = config ? config.shortTimer : process.env.SHORT;
-exports.LONG = config ? config.longTimer : process.env.LONG;
+exports.DISCORD = config ? config.manager.discord : process.env.D_MANAGER;
+exports.GENERAL = config ? config.manager.general : process.env.GENERAL;
+exports.SHORT = config ? config.manager.shortTimer : process.env.SHORT;
+exports.LONG = config ? config.manager.longTimer : process.env.LONG;
