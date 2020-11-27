78547d3df04d0adc2adbd3e753692eb8e4f90526
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-27 19:19:55 -0300

    now exports exphare long timer

--- bots/index.js
+++ bots/index.js
@@ -1,7 +1,9 @@
 const { manager } = require('./manager');
 const { short } = require('./timers/short');
+const { long } = require('./timers/long');
 
 exports.bots = {
     manager: manager,
-    short: short
+    short: short,
+    long: long
 }
