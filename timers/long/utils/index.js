5fe66292006dbb6c6408e1b4c7e593b9caf0db56
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-26 22:02:18 -0300

    setup secrets

--- timers/long/utils/index.js
+++ timers/long/utils/index.js
@@ -5,4 +5,9 @@ try {
     config = null;
 }
 
+exports.GUILD = config ? config.guild : process.env.GUILD;
+exports.ID = config ? config.id.long : process.env.LONG_ID;
 exports.DISCORD = config ? config.discord.long : process.env.D_LONG;
+exports.CHANNEL = config ? config.channels.long : process.env.LONG;
+exports.STATUS = config ? config.channels.longStatus : process.env.LONG_STATUS;
+exports.ROLE = config ? config.roles.long : process.env.LONG_ROLE;
