6b6fd5484bfaf67bc6882ce353d16831c7a5a5db
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-27 20:07:03 -0300

    exports exphare kraken

--- bots/index.js
+++ bots/index.js
@@ -1,9 +1,11 @@
 const { manager } = require('./manager');
 const { short } = require('./timers/short');
 const { long } = require('./timers/long');
+const { kraken } = require('./players/kraken');
 
 exports.bots = {
     manager: manager,
     short: short,
-    long: long
+    long: long,
+    kraken: kraken
 }
