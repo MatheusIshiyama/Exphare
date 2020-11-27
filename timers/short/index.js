7d6e0de5b7dd54338db2e2228da3e5c4e1ca7ef9
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-26 21:33:49 -0300

    finish set timer status

--- timers/short/index.js
+++ timers/short/index.js
@@ -1,46 +1,98 @@
 const Discord = require('discord.js');
-const { DISCORD, CHANNEL } = require('./utils');
+const { GUILD, ID, DISCORD, CHANNEL, STATUS, ROLE } = require('./utils');
 
 const bot = new Discord.Client();
 let state,
+    status,
+    msg,
+    mins,
+    mention,
     connection,
     timer = 1;
 
+const msgEmbed = new Discord.MessageEmbed()
+    .setTitle("Bot inicializando")
+    .setColor("3498DB")
+    .setTimestamp(new Date());
+
 bot.on("ready", async () => {
     console.log(
-        `[Bot] Bot foi iniciado, com ${bot.users.cache.size} usuários, em ${bot.channels.cache.size} canais, em ${bot.guilds.cache.size} servidores.`
+        "[Bot 25/5] Ativo"
     );
     connection = await bot.channels.cache.get(CHANNEL).join();
+    msg = await bot.channels.cache.get(STATUS).send(msgEmbed);
+    msgEmbed.setFooter("Exphare", bot.user.avatarURL());
     state = false;
 });
 
 async function selectState() {
     if(timer === 0) {
-        if (state === false) {
-            timer = 25;
+        if (!state) {
+            timer = 300;
             study();
         } else {
-            timer = 5;
+            timer = 60;
             pause();
         }
         state = !state;
     } else {
+        await description();
+        msg.edit(msgEmbed);
         timer--
     }
 }
 
 async function study() {
-    // const channel = await bot.channels.cache.get(CHANNEL);
+    msgEmbed.setTitle("Hora de estudo!!");
+    status = "[📝]";
+    await description();
+    mention = await bot.channels.cache.get(STATUS).send(ROLE);
+    mention.delete();
+    msg.edit(msgEmbed);
     connection.play('../../assets/study.mp3');
-    console.log("study");
 }
 
 async function pause() {
-    // await bot.channels.cache.get(CHANNEL).setName("Pause");
+    msgEmbed.setTitle("Hora da pausa!!");
+    status = "[✋🏻]";
+    await description();
+    mention = await bot.channels.cache.get(STATUS).send(ROLE);
+    mention.delete();
+    msg.edit(msgEmbed);
     connection.play('../../assets/pause.mp3');
-    console.log("pause");
 }
 
-setInterval(selectState, 1000);
+async function description() {
+    mins = 0;
+    if (timer < 12) {
+        msgEmbed.setDescription(`Tempo restante: ${timer*5} segundos.`);
+        bot.guilds.cache.get(GUILD).members.cache.get(ID).setNickname(`${status} ${timer*5} segs`);
+        await bot.user.setPresence({ activity: { name: `${timer*5} segundos` }});
+    } else if (timer%12 === 0) {
+        if (timer/12 === 1) {
+            msgEmbed.setDescription(`Tempo restante: 1 minuto.`);
+            bot.guilds.cache.get(GUILD).members.cache.get(ID).setNickname(`${status} 1 min`);
+            await bot.user.setPresence({ activity: { name: `1 minuto` }});
+        } else {
+            msgEmbed.setDescription(`Tempo restante: ${timer/12} minutos.`);
+            bot.guilds.cache.get(GUILD).members.cache.get(ID).setNickname(`${status} ${timer/12} mins`);
+            await bot.user.setPresence({ activity: { name: `${timer/12} minutos` }});
+        }
+    } else {
+        for(i = timer; i > 12; i -= 12) {
+            mins++;
+        }
+        msgEmbed.setDescription(`Tempo restante: ${mins} minutos e ${(timer - (mins * 12))*5} segundos.`);
+        if(mins === 1) {
+            bot.guilds.cache.get(GUILD).members.cache.get(ID).setNickname(`${status} ${mins} min e ${(timer - (mins * 12))*5} segs`);
+            await bot.user.setPresence({ activity: { name: `${mins} minutos e ${(timer - (mins * 12))*5} segundos` }});    
+        } else {
+            bot.guilds.cache.get(GUILD).members.cache.get(ID).setNickname(`${status} ${mins} mins e ${(timer - (mins * 12))*5} segs`);
+            await bot.user.setPresence({ activity: { name: `${mins} minutos e ${(timer - (mins * 12))*5} segundos` }});
+        }
+    }
+}
+
+setInterval(selectState, 5000);
 
 bot.login(DISCORD);
