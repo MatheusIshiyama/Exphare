534414e19c86a9a817d667d5267fecff5f57d946
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-27 20:07:37 -0300

    login exphare kraken

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const { bots } = require('./bots/index');
 bots.manager.login(config.manager.token);
 bots.short.login(config.shortTimer.token);
 bots.long.login(config.longTimer.token);
+bots.kraken.login(config.kraken.token);
