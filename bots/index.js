b8a32f36f1bd81d243bc055470ab55308cb4f5ac
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-27 20:54:14 -0300

    exports exphare grifo

--- bots/index.js
+++ bots/index.js
@@ -2,10 +2,12 @@ const { manager } = require('./manager');
 const { short } = require('./timers/short');
 const { long } = require('./timers/long');
 const { kraken } = require('./players/kraken');
+const { grifo } = require('./players/grifo');
 
 exports.bots = {
     manager: manager,
     short: short,
     long: long,
-    kraken: kraken
+    kraken: kraken,
+    grifo: grifo
 }
