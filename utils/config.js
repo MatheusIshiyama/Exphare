c5190bc969d94db220a0fd8ee43dd2fb2563cdd7
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-27 18:37:20 -0300

    finish configs

---
+++ utils/config.js
@@ -0,0 +1,37 @@
+let config;
+try {
+    config = require('../config.json');
+} catch (error) {
+    config = null;
+}
+
+exports.config = {
+    server: config ? config.server : process.env.SERVER,
+    manager: {
+        token: config ? config.bots.manager : process.env.MANAGER_TOKEN
+    },
+    longTimer: {
+        id: config ? config.id.long : process.env.LONG_ID,
+        token: config ? config.bots.long : process.env.LONG_TOKEN,
+        role: config ? config.roles.long : process.env.LONG_ROLE
+    },
+    shortTimer: {
+        id: config ? config.id.short : process.env.SHORT_ID,
+        token: config ? config.bots.short : process.env.SHORT_TOKEN,
+        role: config ? config.roles.short : process.env.SHORT_ROLE
+    },
+    kraken: {
+        token: config ? config.bots.kraken : process.env.KRAKEN_TOKEN
+    },
+    channels: {
+        general: config ? config.channels.generalText : process.env.GENERAL_TEXT,
+        longTimer: {
+            text: config ? config.channels.longText : process.env.LONG_TEXT,
+            voice: config ? config.channels.longVoice : process.env.LONG_VOICE
+        },
+        shortTimer: {
+            text: config ? config.channels.shortText : process.env.SHORT_TEXT,
+            voice: config ? config.channels.shortVoice : process.env.SHORT_VOICE
+        }
+    }
+}
