2f245a8998b69c1f115b7dd8dea6e01d97a9955a
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-26 21:28:16 -0300

    add secrets

--- timers/short/utils/index.js
+++ timers/short/utils/index.js
@@ -5,5 +5,9 @@ try {
     config = null;
 }
 
+exports.GUILD = config ? config.guild : process.env.GUILD;
+exports.ID = config ? config.id.short : process.env.SHORT_ID;
 exports.DISCORD = config ? config.discord.short : process.env.D_SHORT;
 exports.CHANNEL = config ? config.channels.short : process.env.SHORT;
+exports.STATUS = config ? config.channels.shortStatus : process.env.SHORT_STATUS;
+exports.ROLE = config ? config.roles.short : process.env.SHORT_ROLE;
