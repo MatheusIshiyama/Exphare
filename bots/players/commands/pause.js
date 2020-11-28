044f68dd527a69fd29f525f3f9edc8e40ca574dd
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-28 17:02:39 -0300

    finish command

--- bots/players/commands/pause.js
+++ bots/players/commands/pause.js
@@ -1,5 +1,21 @@
-exports.run = message => {
+const msg = require('../../include/message');
 
+exports.run = message => {
+    const queue = message.client.queue.get(message.guild.id);
+    
+    msg.setTitle("Pause");
+    
+    if (!queue) {
+        msg.setDescription("Não estou tocando nada.");
+        return message.channel.send(msg);
+    }
+
+    if (queue.playing) {
+        queue.playing = false;
+        queue.connection.dispatcher.pause(true);
+        msg.setDescription(`${message.author.username} pausou a música.`);
+        return message.channel.send(msg);
+    }
 }
 
 exports.info = {
--- bots/players/commands/resume.js
+++ bots/players/commands/resume.js
@@ -1,5 +1,21 @@
-exports.run = message => {
+const msg = require('../../include/message');
 
+exports.run = message => {
+    const queue = message.client.queue.get(message.guild.id);
+    
+    msg.setTitle("Resume");
+
+    if (!queue) {
+        msg.setDescription("Não estou tocando nada.");
+        return message.channel.send(msg);
+    }
+
+    if (!queue.playing) {
+        queue.playing = true;
+        queue.connection.dispatcher.resume();
+        msg.setDescription(`${message.author.username} ligou a música novamente.`);
+        message.channel.send(msg);
+    }
 }
 
 exports.info = {
