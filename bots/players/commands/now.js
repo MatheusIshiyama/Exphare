1f52555945f370e6f21195f7213dd57a8fba8dd2
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-28 16:39:23 -0300

    finish command

--- bots/players/commands/now.js
+++ bots/players/commands/now.js
@@ -1,5 +1,17 @@
-exports.run = message => {
+const msg = require('../../include/message');
 
+exports.run = message => {
+    const queue = message.client.queue.get(message.guild.id);
+
+    msg.setTitle("Tocando agora");
+
+    if (!queue) {
+        msg.setDescription("Não estou tocando nada.");
+        return message.channel.send(msg);
+    }
+    const song = queue.songs[0];
+    msg.setDescription(`\`${song.title}\`\n${song.url}`);
+    message.channel.send(msg);
 }
 
 exports.info = {
