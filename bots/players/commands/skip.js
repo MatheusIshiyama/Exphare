724df90876aed2f54b4e5f359124f4ce3a260aa6
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-28 17:05:44 -0300

    finish command

--- bots/players/commands/skip.js
+++ bots/players/commands/skip.js
@@ -1,5 +1,19 @@
-exports.run = (message) => {
+const msg = require('../../include/message');
 
+exports.run = (message) => {
+    const queue = message.client.queue.get(message.guild.id);
+    
+    msg.setTitle("Skip");
+
+    if (!queue) {
+        msg.setDescription("Não estou tocando nada");
+        return message.channel.send(msg);
+    }
+
+    queue.playing = true;
+    queue.connection.dispatcher.end();
+    msg.setDescription(`${message.author.username} passou de música.`);
+    message.channel.send(msg);
 }
 
 exports.info = {
