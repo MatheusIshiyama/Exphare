992dfcb7a906121efd8d8a410c0405864a6ad4f8
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-28 13:43:43 -0300

    login manager bot first than others

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { config } = require('./utils/config');
 const { bots } = require('./bots/index');
 
-bots.manager.login(config.manager.token);
-bots.short.login(config.shortTimer.token);
-bots.long.login(config.longTimer.token);
-bots.kraken.login(config.kraken.token);
-bots.grifo.login(config.grifo.token);
+bots.manager.login(config.manager.token).then(() => {
+    bots.short.login(config.shortTimer.token);
+    bots.long.login(config.longTimer.token);
+    bots.kraken.login(config.kraken.token);
+    bots.grifo.login(config.grifo.token);
+});
