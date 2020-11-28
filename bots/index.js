58d980be4a42ce489a776ef4eff5a2af97823cdc
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-28 17:41:23 -0300

    exports exphare - valkyrie

--- bots/index.js
+++ bots/index.js
@@ -3,11 +3,13 @@ const { short } = require('./timers/short');
 const { long } = require('./timers/long');
 const { kraken } = require('./players/kraken');
 const { grifo } = require('./players/grifo');
+const { valkyrie } = require("./players/valkyrie");
 
 exports.bots = {
     manager: manager,
     short: short,
     long: long,
     kraken: kraken,
-    grifo: grifo
+    grifo: grifo,
+    valkyrie: valkyrie
 }
