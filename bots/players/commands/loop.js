f0ed3d5d306b574732fd34c5e7f6bdb4e67920d9
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-28 16:57:00 -0300

    finish command

--- bots/players/commands/loop.js
+++ bots/players/commands/loop.js
@@ -1,5 +1,18 @@
-exports.run = message => {
+const msg = require('../../include/message');
 
+exports.run = message => {
+    const queue = message.client.queue.get(message.guild.id);
+
+    msg.setTitle("Loop");
+
+    if (!queue) {
+        msg.setDescription("Não estou tocando nada.");
+        return message.channel.send(msg);
+    } else {
+        queue.loop = !queue.loop;
+        msg.setDescription(`O loop está \`${queue.loop ? "ligado" : "desligado"}\``);
+        return message.channel.send(msg);
+    }
 }
 
 exports.info = {
