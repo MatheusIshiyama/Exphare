e53c1051eee4281928759520ca6ca90e07d53676
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-30 23:24:35 -0300

    add function welcome

--- bots/manager.js
+++ bots/manager.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
-const Canvas = require('canvas');
 const { config } = require('../utils/config');
+const { welcome } = require('./include/welcome');
 const { role } = require('./include/channel');
 const { help } = require('./include/help');
 
@@ -56,55 +56,7 @@ bot.on("messageReactionRemove", async (reaction, user) => {
 });
 
 bot.on("guildMemberAdd", async member => {
-    const channel = member.guild.channels.cache.find(channel => channel.name === 'boas-vindas');
-	if (!channel) return;
-    
-    const canvas = Canvas.createCanvas(1920, 1080);
-    const ctx = canvas.getContext('2d');
-
-    // * add fonte
-    Canvas.registerFont("assets/SansitaSwashed.ttf", { family: 'Sansita Swashed' });
-
-    // * background
-    const background = await Canvas.loadImage('./assets/background.jpg');
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-    // * sombra
-    ctx.beginPath();
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
-    ctx.fillRect(50, 50, 1820, 980);
-    ctx.stroke();
-
-    // * contorno da foto de perfil
-    ctx.beginPath();
-    ctx.strokeStyle = 'rgba(255, 255, 255, 0.35)';
-    ctx.lineWidth = 60;
-    ctx.arc(960, 350, 200, 0, Math.PI * 2, true);
-    ctx.stroke();
-    
-    // * boas vindas e user
-    ctx.fillStyle = 'rgb(48, 55, 101)';
-    ctx.strokeStyle = 'rgb(255, 255, 255)';
-    ctx.lineWidth = 20;
-    ctx.textAlign = 'center';
-    ctx.font = '110px Sansita Swashed';
-    ctx.strokeText("Bem vindo(a) ao Exphare", 960, 750);
-    ctx.fillText("Bem vindo(a) ao Exphare", 960, 750);
-    ctx.font = '90px Sansita Swashed';
-    ctx.strokeText(member.user.tag, 960, 900);
-    ctx.fillText(member.user.tag, 960, 900);
-
-    // * foto de perfil
-    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpeg' }));
-    ctx.beginPath();
-	ctx.arc(960, 350, 200, 0, Math.PI * 2, true);
-	ctx.closePath();
-	ctx.clip()
-    ctx.drawImage(avatar, 760, 150, 400, 400);
-
-    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome-image.png");
-
-    channel.send(`Bem vindo(a) ${member.user} ao Exphare!`, attachment);
+    welcome(member);
 });
 
 bot.on("voiceStateUpdate", async (oldState, newState) => {
