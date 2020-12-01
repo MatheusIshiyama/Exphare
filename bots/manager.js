7a9e8336d878a995df54eb82c18ae01f333c7f14
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-30 23:13:21 -0300

    add function role

--- bots/manager.js
+++ bots/manager.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const Canvas = require('canvas');
 const { config } = require('../utils/config');
+const { role } = require('./include/channel');
 
 const bot = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
 
@@ -105,57 +106,8 @@ bot.on("guildMemberAdd", async member => {
     channel.send(`Bem vindo(a) ${member.user} ao Exphare!`, attachment);
 });
 
-bot.on("voiceStateUpdate", async voice => {
-
-    let roleInfo,
-        role = {
-            id: null,
-            name: null
-        },
-        channel = {
-            id: null,
-            name: null
-        },
-        user = {
-            id: null,
-            channel: null
-        };
-
-    const userReq = await voice.guild.voiceStates.cache.filter(user => user.id === voice.id);
-
-    userReq.map(userInfo => {
-        user.id = userInfo.id;
-        user.channel = userInfo.channelID;
-    })
-
-    const channelReq = await voice.guild.channels.cache.filter(channels => channels.id === user.channel);
-    channelReq.map(channelInfo => {
-        channel.id = channelInfo.id,
-        channel.name = channelInfo.name
-    });
-    
-    if (channel.name) {
-        role = channel;
-    } else {
-        role.id = voice.channel.id;
-        role.name = voice.channel.name;
-    }
-    
-    if (role.id === config.channels.general) {
-        roleInfo = voice.guild.roles.cache.find(roles => roles.name === "geral");
-    } else if (role.id === config.channels.shortTimer.voice) {
-        roleInfo = voice.guild.roles.cache.find(roles => roles.name === "25 por 5");
-    } else if (role.id === config.channels.longTimer.voice) {
-        roleInfo = voice.guild.roles.cache.find(roles => roles.name === "50 por 10");
-    } else {
-        roleInfo = voice.guild.roles.cache.find(roles => roles.name === role.name);
-    }
-
-    if (user.channel) {
-        voice.guild.members.cache.get(user.id).roles.add(roleInfo)
-    } else {
-        voice.guild.members.cache.get(user.id).roles.remove(roleInfo)
-    }
+bot.on("voiceStateUpdate", async (oldState, newState) => {
+    role(oldState);
 });
 
 bot.on("message", async message => {
