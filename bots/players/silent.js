8bb6ac591e618e492091695ca2bd0ee94cc2b3a6
Matheus Ishiyama <semrumo3@hotmail.com>
2020-12-02 15:57:38 -0300

    now play lofi on silent channel when bot login

--- bots/players/silent.js
+++ bots/players/silent.js
@@ -1,9 +1,16 @@
 const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+const { config } = require('../../utils/config');
 
 const bot = new Discord.Client();
 
-bot.on("ready", () => {
+bot.on("ready", async () => {
     console.log("[Bot silent] Ativo");
 })
+
+bot.setTimeout(async () => {
+    const channel = await bot.channels.cache.get(config.channels.silent).join();
+    channel.play(ytdl("https://www.youtube.com/watch?v=5qap5aO4i9A"));
+}, 3000);
 
 exports.silent = bot;
