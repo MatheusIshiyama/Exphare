e479c4253503cd7b0ddae539bb1174905d88082d
Matheus Ishiyama <semrumo3@hotmail.com>
2020-12-07 20:13:27 -0300

    simplify config

--- utils/config.js
+++ utils/config.js
@@ -1,31 +1,17 @@
 let config;
 try {
-    config = require('../config.json');
+    config = require("../config.json");
 } catch (error) {
     config = null;
 }
 
 exports.config = {
     mongodb: config ? config.mongodb : process.env.MONGO_DB,
-    manager: {
-        token: config ? config.bots.manager : process.env.MANAGER_TOKEN
-    },
-    silent: {
-        token: config ? config.bots.silent : process.env.SILENT_TOKEN
-    },
-    longTimer: {
-        token: config ? config.bots.long : process.env.LONG_TOKEN
-    },
-    shortTimer: {
-        token: config ? config.bots.short : process.env.SHORT_TOKEN
-    },
-    kraken: {
-        token: config ? config.bots.kraken : process.env.KRAKEN_TOKEN
-    },
-    grifo: {
-        token: config ? config.bots.grifo : process.env.GRIFO_TOKEN
-    },
-    valkyrie: {
-        token: config ? config.bots.valkyrie : process.env.VALKYRIE_TOKEN
-    }
-}
+    manager: config ? config.bots.manager : process.env.MANAGER_TOKEN,
+    silent: config ? config.bots.silent : process.env.SILENT_TOKEN,
+    longTimer: config ? config.bots.long : process.env.LONG_TOKEN,
+    shortTimer: config ? config.bots.short : process.env.SHORT_TOKEN,
+    kraken: config ? config.bots.kraken : process.env.KRAKEN_TOKEN,
+    grifo: config ? config.bots.grifo : process.env.GRIFO_TOKEN,
+    valkyrie: config ? config.bots.valkyrie : process.env.VALKYRIE_TOKEN,
+};
