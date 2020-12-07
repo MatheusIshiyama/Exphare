62a14f6fc5002f349aae48134abe5ae22000a979
Matheus Ishiyama <semrumo3@hotmail.com>
2020-12-07 20:13:53 -0300

    use new config util

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const { config } = require('./utils/config');
 const { bots } = require('./bots/index');
 
-bots.manager.login(config.manager.token).then(() => {
-    bots.short.login(config.shortTimer.token);
-    bots.long.login(config.longTimer.token);
-    bots.kraken.login(config.kraken.token);
-    bots.grifo.login(config.grifo.token);
-    bots.valkyrie.login(config.valkyrie.token);
-    bots.silent.login(config.silent.token);
+bots.manager.login(config.manager).then(() => {
+    bots.short.login(config.shortTimer);
+    bots.long.login(config.longTimer);
+    bots.kraken.login(config.kraken);
+    bots.grifo.login(config.grifo);
+    bots.valkyrie.login(config.valkyrie);
+    bots.silent.login(config.silent);
 });
 
 mongoose
