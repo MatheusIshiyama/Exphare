c778aef78a8ac20aa4901dfd479a0b45afacc476
Matheus Ishiyama <semrumo3@hotmail.com>
2020-12-07 19:43:43 -0300

    remove config utils and set channels ids

--- bots/manager.js
+++ bots/manager.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const { config } = require('../utils/config');
 const { welcome } = require('./include/welcome');
 const { role } = require('./include/channel');
 const { userConnection, showTasks, newTask, removeTask } = require('./include/user');
@@ -8,18 +7,24 @@ const { help } = require('./include/help');
 const bot = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
 
 let presence = false;
-const longChannel = config.channels.longTimer.text;
-const shortChannel = config.channels.shortTimer.text;
+const botsIds = [
+    '781680031624855582', // * longTimer
+    '781517289685778455', // * shortTimer
+    '781872955298152491', // * kraken
+    '782030076278472734', // * grifo
+    '782344032264126464', // * valkyrie
+    '783693873674977292', // * silent
+];
 
 bot.once("ready", () => {
     console.log("[Bot Manager] Ativo");
     bot.user.setPresence({ activity: { name: "lo-fi | precisa de ajuda? digite !comandos", type: 2 }});
-    bot.channels.cache.get(longChannel).messages
+    bot.channels.cache.get('781681804008357888').messages
         .fetch()
         .then(msg =>
             msg.forEach(msg => msg.delete())
         );
-    bot.channels.cache.get(shortChannel).messages
+    bot.channels.cache.get('782269834750197770').messages
         .fetch()
         .then(msg =>
             msg.forEach(msg => msg.delete())
@@ -35,7 +40,7 @@ bot.on("messageReactionAdd", async (reaction, user) => {
 			return;
 		}
     }
-	if(reaction.message.channel.messages.cache.get(config.confirm)) {
+	if(reaction.message.channel.messages.cache.get('782770889325936640')) {
         const role = reaction.message.guild.roles.cache.find(role => role.name === "membro");
         reaction.message.guild.members.cache.get(user.id).roles.add(role);
     }
@@ -50,7 +55,7 @@ bot.on("messageReactionRemove", async (reaction, user) => {
 			return;
 		}
 	}
-	if(reaction.message.channel.messages.cache.get(config.confirm)) {
+	if(reaction.message.channel.messages.cache.get('782770889325936640')) {
         const role = reaction.message.guild.roles.cache.find(role => role.name === "membro");
         reaction.message.guild.members.cache.get(user.id).roles.remove(role);
     }
@@ -61,12 +66,7 @@ bot.on("guildMemberAdd", async member => {
 });
 
 bot.on("voiceStateUpdate", async (oldState, newState) => {
-    if (newState.id === config.longTimer.id) return;
-    if (newState.id === config.shortTimer.id) return;
-    if (newState.id === config.grifo.id) return;
-    if (newState.id === config.kraken.id) return;
-    if (newState.id === config.valkyrie.id) return;
-    if (newState.id === config.silent.id) return;
+    botsIds.forEach(botId => { if(newState.id === botId) return });
     role(oldState);
     userConnection(newState);
 });
@@ -74,14 +74,13 @@ bot.on("voiceStateUpdate", async (oldState, newState) => {
 bot.on("message", async message => {
     if (message.author.bot) return;
     const id = message.channel.id;
-    if(id === config.channels.toDo) {
+    if(id === '783434206697095198') {
         showTasks(message);
-    } else if (id === config.channels.add) {
-            newTask(message);
-    } else if (id === config.channels.remove) {
-            removeTask(message);
-    };
-    if (message.content === "!comandos") {
+    } else if (id === '783395377557929984') {
+        newTask(message);
+    } else if (id === '783395951527067679') {
+        removeTask(message);
+    } else if (message.content === "!comandos") {
         help(message);
     }
 });
