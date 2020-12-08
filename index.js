d61cd0415fc52387cb93ae4f618fd50eda7d845a
Matheus Ishiyama <semrumo3@hotmail.com>
2020-12-08 13:07:53 -0300

    require database

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-const mongoose = require('mongoose');
-const { config } = require('./utils/config');
-require('./bots/index');
-
-mongoose
-    .connect(config.mongodb, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false })
-    .then(console.log("[MongoDB] conectado ao mongo"))
+require('./database')
+require('./bots');
