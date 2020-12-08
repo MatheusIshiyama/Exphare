794851782c0f196cd60d38eb2fd833762892b9e0
Matheus Ishiyama <semrumo3@hotmail.com>
2020-12-08 12:46:07 -0300

    just require bots

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 const mongoose = require('mongoose');
 const { config } = require('./utils/config');
-const { bots } = require('./bots/index');
-
-bots.manager.login(config.manager).then(() => {
-    bots.short.login(config.shortTimer);
-    bots.long.login(config.longTimer);
-    bots.kraken.login(config.kraken);
-    bots.grifo.login(config.grifo);
-    bots.valkyrie.login(config.valkyrie);
-    bots.silent.login(config.silent);
-});
+require('./bots/index');
 
 mongoose
     .connect(config.mongodb, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false })
