6de9a020b3d64f5b2bba9ddc0614ca69e9d89f2c
Matheus Ishiyama <semrumo3@hotmail.com>
2020-12-08 13:38:52 -0300

    create discord strategy

---
+++ server/strategies/discordStrategy.js
@@ -0,0 +1,27 @@
+const DiscordStrategy = require('passport-discord').Strategy;
+const userModel = require('../../models/user');
+const passport = require('passport');
+const { config } = require('../../utils/config');
+
+passport.use(new DiscordStrategy({
+    clientID: config.client.id,
+    clientSecret: config.client.secret,
+    callbackURL: '/auth/redirect',
+    scope: ['identify']
+}, async (accessToken, refreshToken, profile, done) => {
+    const userRequest = await userModel.findOne({ id: profile.id });
+    if(!userRequest) {
+        const newUser = new userModel({
+            id: profile.id,
+            username: profile.username,
+            discriminator: profile.discriminator,
+            lastConnection: 0,
+            accumulatedTime: 0,
+            toDo: Array
+        });
+        const savedUser = await newUser.save();
+        done(null, savedUser);
+    } else {
+        done(null, userRequest);
+    }
+}))
