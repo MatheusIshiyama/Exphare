2a655b45536fc5df4b92829cf1a0a93243a06939
Matheus Ishiyama <semrumo3@hotmail.com>
2020-12-08 14:51:06 -0300

    change successRedirect

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 
 router.get('/', passport.authenticate('discord'));
 router.get('/redirect', passport.authenticate('discord', {
-    successRedirect: '/dashboard',
+    successRedirect: '/register',
     failureRedirect: '/error'
 }));
 
