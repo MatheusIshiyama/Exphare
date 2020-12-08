d9d851e9c815abc59e084a188611b478ed72ca7a
Matheus Ishiyama <semrumo3@hotmail.com>
2020-12-08 15:52:57 -0300

    get user info

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,3 +1,17 @@
 const router = require('express').Router();
+const userModel = require('../../models/user');
+
+function isAuthorizated(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/')
+    }
+}
+
+router.get('/', isAuthorizated, async (req, res) => {
+    const user = await userModel.findOne({ id: req.user.id });
+    res.send(user);
+})
 
 module.exports = router;
