01036666f0c3c309d46a964c8aa17b2f6069f414
Matheus Ishiyama <semrumo3@hotmail.com>
2020-12-08 14:22:38 -0300

    set discord oauth2

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,10 @@
 const router = require('express').Router();
+const passport = require('passport');
 
+router.get('/', passport.authenticate('discord'));
+router.get('/redirect', passport.authenticate('discord', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/error'
+}));
 
 module.exports = router;
