eca28082432da6a8a699a6b3cf82f2af39827faa
Matheus Ishiyama <semrumo3@hotmail.com>
2020-12-08 23:13:20 -0300

    verify if password is valid and checked

--- server/public/scripts/register.js
+++ server/public/scripts/register.js
@@ -2,8 +2,10 @@ var password = document.getElementById('password');
 var check_password = document.getElementById('check_password');
 
 function validatePassword() {
-    if (password.value != check_password.value) {
-        check_password.setCustomValidity('As senhas sao diferentes');
+    if (password.value.length < 9) {
+        password.setCustomValidity('A senha deve conter pelo menos 8 caracteres.');
+    } else if (password.value != check_password.value) {
+        check_password.setCustomValidity('As senhas são diferentes!');
     }
 };
 
