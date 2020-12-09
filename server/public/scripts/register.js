b93916f3128fa1d2b25c69e693fabe436d0c5204
Matheus Ishiyama <semrumo3@hotmail.com>
2020-12-08 23:32:12 -0300

    fix validatePassword

--- server/public/scripts/register.js
+++ server/public/scripts/register.js
@@ -2,12 +2,17 @@ var password = document.getElementById('password');
 var check_password = document.getElementById('check_password');
 
 function validatePassword() {
-    if (password.value.length < 9) {
+    if (password.value.length < 8) {
         password.setCustomValidity('A senha deve conter pelo menos 8 caracteres.');
+        check_password.setCustomValidity('');
     } else if (password.value != check_password.value) {
         check_password.setCustomValidity('As senhas são diferentes!');
+        password.setCustomValidity('');
+    } else {
+        password.setCustomValidity('');
+        check_password.setCustomValidity('');
     }
 };
 
 password.onchange = validatePassword;
-check_password.onchange = validatePassword;
+check_password.onkeyup = validatePassword;
