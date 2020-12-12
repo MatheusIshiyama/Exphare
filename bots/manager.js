1e4876ef97e007f01107a2ebd0f74bdf65c8f0b4
Matheus Ishiyama <semrumo3@hotmail.com>
2020-12-11 21:03:43 -0300

    implement setUsername function

--- bots/manager.js
+++ bots/manager.js
@@ -1,95 +1,122 @@
-const Discord = require('discord.js');
-const { welcome } = require('./include/welcome');
-const { role } = require('./include/channel');
-const { userConnection, showTasks, newTask, removeTask, getProfile } = require('./controller/user');
-const { help } = require('./include/help');
+const Discord = require("discord.js");
+const { welcome } = require("./include/welcome");
+const { role } = require("./include/channel");
+const {
+    userConnection,
+    showTasks,
+    newTask,
+    removeTask,
+    getProfile,
+    setUsername,
+} = require("./controller/user");
+const { help } = require("./include/help");
 
-const bot = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
+const bot = new Discord.Client({
+    partials: ["MESSAGE", "CHANNEL", "REACTION"],
+});
 
 let presence = false;
 const botsIds = [
-    '781680031624855582', // * longTimer
-    '781517289685778455', // * shortTimer
-    '781872955298152491', // * kraken
-    '782030076278472734', // * grifo
-    '782344032264126464', // * valkyrie
-    '783693873674977292', // * silent
+    "781680031624855582", // * longTimer
+    "781517289685778455", // * shortTimer
+    "781872955298152491", // * kraken
+    "782030076278472734", // * grifo
+    "782344032264126464", // * valkyrie
+    "783693873674977292", // * silent
 ];
 
 bot.once("ready", () => {
     console.log("[Bot Manager] Ativo");
-    bot.user.setPresence({ activity: { name: "lo-fi | precisa de ajuda? digite !comandos", type: 2 }});
-    bot.channels.cache.get('781681804008357888').messages
-        .fetch()
-        .then(msg =>
-            msg.forEach(msg => msg.delete())
-        );
-    bot.channels.cache.get('782269834750197770').messages
-        .fetch()
-        .then(msg =>
-            msg.forEach(msg => msg.delete())
-        );
+    bot.user.setPresence({
+        activity: {
+            name: "lo-fi | precisa de ajuda? digite !comandos",
+            type: 2,
+        },
+    });
+    bot.channels.cache
+        .get("781681804008357888")
+        .messages.fetch()
+        .then((msg) => msg.forEach((msg) => msg.delete()));
+    bot.channels.cache
+        .get("782269834750197770")
+        .messages.fetch()
+        .then((msg) => msg.forEach((msg) => msg.delete()));
 });
 
 bot.on("messageReactionAdd", async (reaction, user) => {
-	if (reaction.partial) {
-		try {
-			await reaction.fetch();
-		} catch (error) {
-			console.error('Something went wrong when fetching the message: ', error);
-			return;
-		}
+    if (reaction.partial) {
+        try {
+            await reaction.fetch();
+        } catch (error) {
+            console.error(
+                "Something went wrong when fetching the message: ",
+                error
+            );
+            return;
+        }
     }
-	if(reaction.message.channel.messages.cache.get('782770889325936640')) {
-        const role = reaction.message.guild.roles.cache.find(role => role.name === "membro");
+    if (reaction.message.channel.messages.cache.get("782770889325936640")) {
+        const role = reaction.message.guild.roles.cache.find(
+            (role) => role.name === "membro"
+        );
         reaction.message.guild.members.cache.get(user.id).roles.add(role);
     }
 });
 
 bot.on("messageReactionRemove", async (reaction, user) => {
     if (reaction.partial) {
-		try {
-			await reaction.fetch();
-		} catch (error) {
-			console.error('Something went wrong when fetching the message: ', error);
-			return;
-		}
-	}
-	if(reaction.message.channel.messages.cache.get('782770889325936640')) {
-        const role = reaction.message.guild.roles.cache.find(role => role.name === "membro");
+        try {
+            await reaction.fetch();
+        } catch (error) {
+            console.error(
+                "Something went wrong when fetching the message: ",
+                error
+            );
+            return;
+        }
+    }
+    if (reaction.message.channel.messages.cache.get("782770889325936640")) {
+        const role = reaction.message.guild.roles.cache.find(
+            (role) => role.name === "membro"
+        );
         reaction.message.guild.members.cache.get(user.id).roles.remove(role);
     }
 });
 
-bot.on("guildMemberAdd", async member => {
+bot.on("guildMemberAdd", async (member) => {
     welcome(member);
 });
 
 bot.on("voiceStateUpdate", async (oldState, newState) => {
     let isBot = false;
-    botsIds.forEach(botId => { if(newState.id === botId) isBot = true });
-    if(isBot) return;
+    botsIds.forEach((botId) => {
+        if (newState.id === botId) isBot = true;
+    });
+    if (isBot) return;
     role(oldState);
     userConnection(newState);
 });
 
-bot.on("message", async message => {
+bot.on("message", async (message) => {
     const id = message.channel.id;
     if (message.author.bot) return;
-    if (message.channel.type === 'dm') { 
-        if (message.content === 'profile') {
-            getProfile(message);    
-        } else if (message.content === 'username') {
-            
+    if (message.channel.type === "dm") {
+        if (message.content === "profile") {
+            getProfile(message);
+        } else if (message.content === "username") {
+            const arg = message.content
+                .trim()
+                .split(/ +/g);
+            let username = arg.shift();
+            setUsername(message, username);
         } else {
             return;
         }
-        
-    } else if(id === '783434206697095198') {
+    } else if (id === "783434206697095198") {
         showTasks(message);
-    } else if (id === '783395377557929984') {
+    } else if (id === "783395377557929984") {
         newTask(message);
-    } else if (id === '783395951527067679') {
+    } else if (id === "783395951527067679") {
         removeTask(message);
     } else if (message.content === "!comandos") {
         help(message);
@@ -98,12 +125,19 @@ bot.on("message", async message => {
 
 async function botPresence() {
     presence = !presence;
-    if(presence) {
-        bot.user.setPresence({ activity: { name: "Exphare deseja bons estudos a todos!"}});
+    if (presence) {
+        bot.user.setPresence({
+            activity: { name: "Exphare deseja bons estudos a todos!" },
+        });
     } else {
-        bot.user.setPresence({ activity: { name: "lo-fi | precisa de ajuda? digite !comandos", type: 2 }});
+        bot.user.setPresence({
+            activity: {
+                name: "lo-fi | precisa de ajuda? digite !comandos",
+                type: 2,
+            },
+        });
     }
-};
+}
 
 setInterval(botPresence, 11000);
 
